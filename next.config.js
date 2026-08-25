6a21d195ddbb06b6fe754df3f4eba6731f12f0c1
lilianeuwase <38309956+lilianeuwase@users.noreply.github.com>
2026-08-25 14:28:35 +0200

    configure static Cloudflare deployment

--- next.config.js
+++ next.config.js
@@ -2,12 +2,6 @@
 const nextConfig = {
   output: 'export',
   images: { unoptimized: true },
-  swcMinify: false,
-  webpack: (config) => {
-    // 👇 turn off CSS minimization completely
-    config.optimization.minimize = false;
-    return config;
-  },
   typescript: {
     // WARNING: allows deploying with TypeScript errors — use only temporarily
     ignoreBuildErrors: true,
